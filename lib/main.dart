35b2b3802858420093a25adabd1da2559bd18c10
kysk2295 <38239554+kysk2295@users.noreply.github.com>
2021-12-29 03:11:01 +0900

    book register success

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:untitled2/MainPage.dart';
+import 'package:untitled2/screen/home_screen.dart';
 import 'package:untitled2/screen/login_screen.dart';
 
-void main() {
-  runApp(MaterialApp(home: LoginScreen()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(home: MainPage()));
 }
 
 class MyApp extends StatefulWidget {
@@ -11,39 +16,13 @@ class MyApp extends StatefulWidget {
 }
 class _MyAppState extends State<MyApp>{
 
-  GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+
+
 
   @override
   Widget build(BuildContext context) {
-
-    GoogleSignInAccount? user = _googleSignIn.currentUser;
     return MaterialApp(
       title: "BookToBook",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Google Sign in (Signed ' + (user ==null ? 'out' : 'in')+')'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(onPressed:  user!=null ? null : () async {
-
-              await _googleSignIn.signIn();
-              setState(() {
-
-              });
-            }, child: Text('Sign in')),
-            ElevatedButton(onPressed: user == null ? null : () async {
-
-              await _googleSignIn.signOut();
-              setState(() {
-
-              });
-            }, child: Text('Sign out'))
-          ],
-        ),
-      ),
-    ),
     );
   }
 }
