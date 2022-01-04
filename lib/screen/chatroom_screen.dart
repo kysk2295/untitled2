1870e1902e801f952b589a67a576fdfa6ba879a1
kysk2295 <38239554+kysk2295@users.noreply.github.com>
2022-01-04 21:16:58 +0900

    chat 끝! cloud functions 성공! fcm 성공!

--- lib/screen/chatroom_screen.dart
+++ lib/screen/chatroom_screen.dart
@@ -25,14 +25,6 @@ class _ChatRoomScreenState extends State<ChatRoomScreen>{
   late String fcmToken;
 
 
-  // List<ChatMessage> messages = [
-  //   ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
-  //   ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
-  //   ChatMessage(messageContent: "Hey Kriss, I am doing fine dude. wbu?", messageType: "sender"),
-  //   ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
-  //   ChatMessage(messageContent: "Is there any thing wrong?", messageType: "sender"),
-  // ];
-
   final List<ChatMessage> messages=[];
   late FirebaseMessaging firebaseMessaging;
 
